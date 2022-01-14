031820d2d86f818bdb4bbf10d591a2e09c7f1b84
ElbruSuleymanli <72246279+elbrusuleymanli@users.noreply.github.com>
2022-01-14 14:11:15 +0400

    Add files via upload

---
+++ MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
